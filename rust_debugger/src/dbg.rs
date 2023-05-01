ff211fc8b7b21906d0e2b3c701906f8d0683db63
atsushi421 <yff81986@nifty.com>
2023-05-02 07:01:31 +0900

    feat: add common methods

--- rust_debugger/src/dbg.rs
+++ rust_debugger/src/dbg.rs
@@ -10,4 +10,95 @@ use nix::{
 };
 use std::ffi::{c_void, CString};
 
-todo!();
+/// デバッガ内の情報
+pub struct DbgInfo {
+    pid: Pid,
+    brk_addr: Option<*mut c_void>, // ブレークポイントのアドレス
+    brk_val: i64,                  // ブレークポイントを設定したメモリの元の値
+    filename: String,              // 実行ファイル
+}
+
+/// デバッガ
+/// ZDbg<Running>は子プロセスを実行中
+/// ZDbg<NotRunning>は子プロセスは実行していない
+pub struct ZDbg<T> {
+    info: Box<DbgInfo>,
+    _state: T,
+}
+
+/// デバッガの実装
+pub struct Running; // 実行中
+pub struct NotRunning; // 実行していない
+
+/// デバッガの実装の列挙型表現。Exitの場合終了
+pub enum State {
+    Running(ZDbg<Running>),
+    NotRunning(ZDbg<NotRunning>),
+    Exit,
+}
+
+/// RunningとNotRunningで共通の実装
+impl<T> ZDbg<T> {
+    /// ブレークポイントのアドレスを設定する関数。子プロセスのメモリ上には反映しない。
+    /// アドレス設定に成功した場合はtrueを返す
+    fn set_break_addr(&mut self, cmd: &[&str]) -> bool {
+        if self.info.brk_addr.is_some() {
+            eprintln!(
+                "<<ブレークポイントは設定済みです : Addr = {:p}>>",
+                self.info.brk_addr.unwrap()
+            );
+            false
+        } else if let Some(addr) = get_break_addr(cmd) {
+            self.info.brk_addr = Some(addr); // ブレークポイントのアドレスを保存
+            true
+        } else {
+            false
+        }
+    }
+
+    /// 共通のコマンドを実行
+    fn do_cmd_common(&self, cmd: &[&str]) {
+        match cmd[0] {
+            "help" | "h" => do_help(),
+            _ => (),
+        }
+    }
+}
+
+/// コマンドからブレークポイントを計算
+fn get_break_addr(cmd: &[&str]) -> Option<*mut c_void> {
+    if cmd.len() < 2 {
+        eprintln!("<<アドレスを指定してください\n例 : break 0x8000>>");
+        return None;
+    }
+
+    let addr_str = cmd[1];
+    if &addr_str[0..2] != "0x" {
+        eprintln!("<<アドレスは16進数でのみ指定可能です\n例 : break 0x8000>>");
+        return None;
+    }
+
+    let addr = match usize::from_str_radix(&addr_str[2..], 16) {
+        Ok(addr) => addr,
+        Err(e) => {
+            eprintln!("<<アドレス変換エラー : {}>>", e);
+            return None;
+        }
+    } as *mut c_void;
+
+    Some(addr)
+}
+
+/// ヘルプを表示
+fn do_help() {
+    println!(
+        r#"コマンド一覧 (括弧内は省略記法)
+break 0x8000 : ブレークポイントを0x8000番地に設定 (b 0x8000)
+run          : プログラムを実行 (r)
+continue     : プログラムを再開 (c)
+stepi        : 機械語レベルで1ステップ実行 (s)
+registers    : レジスタを表示 (regs)
+exit         : 終了
+help         : このヘルプを表示 (h)"#
+    );
+}
--- rust_debugger/src/main.rs
+++ rust_debugger/src/main.rs
@@ -1,3 +1,5 @@
+pub mod dbg;
+
 fn main() {
     println!("Hello, world!");
 }
